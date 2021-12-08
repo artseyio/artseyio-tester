4e6d6ddcde389dfae0203d753bda6c7c8a5e0443
Gerrit Gazic <geaz@users.noreply.github.com>
2021-12-08 19:27:09 +0100

    Add info text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <img src={logo} alt="logo" id="logo" />
             <h1>ARTSEY Tester</h1>
             <p>
-                On this site you're able to test the great ARTSEY layout. Without the need of a dedicated keyboard.<br/>
+                On this site you're able to test the great ARTSEY layout. Without the need of a dedicated keyboard. The tester supports all alpha key combos, space and backspace.<br/>
                 To learn more about ARTSEY visit the <a href="https://artsey.io" title="ARTSEY Website" target="_blank" rel="noreferrer">website</a>.<br/>
                 <a href="https://raw.githubusercontent.com/artseyio/artsey/main/layout%20diagrams/current.jpg" title="ARTSEY Cheatsheet">Cheatsheet</a> - <a href="Learning_Artsey.pdf" title="Learn ARTSEY Book">Learn ARTSEY Book</a>
             </p>
